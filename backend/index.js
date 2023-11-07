a0f4fed851b07175eecac63a3d199fcd98394ec1
kobby00 <agyeis6000@gmail.com>
2023-11-07 09:09:46 -0200

    update on cors and mabile view changes on landing page

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.JWT_SECRET; 
 const app = express();
-app.use(cors({credentials:true, origin:"https://calm-salmiakki-706c96.netlify.app"}));
+app.use(cors({credentials:true, origin:"https://bucolic-granita-dd098f.netlify.app"}));
 app.use(express.json());
 app.use(cookieParser());
 
